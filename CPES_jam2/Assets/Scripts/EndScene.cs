a7e7562640bcb794a775cbd7f2b8ef78be0bb64f
Alrefai02 <taleb.alrefai2002@gmail.com>
2022-07-23 15:57:42 +0300

    bababoi

---
+++ CPES_jam2/Assets/Scripts/EndScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(goToMain());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator goToMain()
+    {
+        yield return new WaitForSeconds(6f);
+        SceneManager.LoadScene(17);
+    }
+}
---
+++ CPES_jam2/Assets/Scripts/StartLevel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartLevel : MonoBehaviour
+{
+     public void LevelStart()
+     {
+        SceneManager.LoadScene(0);    
+     }
+}
